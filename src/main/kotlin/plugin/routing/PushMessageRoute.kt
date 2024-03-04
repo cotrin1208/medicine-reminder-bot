31400e35cf5ea3c582e064206d9467b9e92e4606
cotrin1208 <gummy8672@gmail.com>
2024-03-04 12:08:29 +0900

    fix: プッシュメッセージ送信エンドポイントの修正

--- src/main/kotlin/plugin/routing/PushMessageRoute.kt
+++ src/main/kotlin/plugin/routing/PushMessageRoute.kt
@@ -1,6 +1,9 @@
 package io.github.cotrin1208.plugin.routing
 
 import io.github.cotrin1208.service.IMessageService
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
 
@@ -10,23 +13,29 @@ fun Route.pushMessageRoute() {
     route("notify") {
         post("friday-morning") {
             messageService.sendFridayMorningReminder()
+            call.respond(HttpStatusCode.OK)
         }
         post("friday-evening") {
             messageService.sendFridayEveningReminder()
+            call.respond(HttpStatusCode.OK)
         }
         post("sunday-morning") {
             messageService.sendSundayMorningReminder()
+            call.respond(HttpStatusCode.OK)
         }
     }
     route("check") {
         post("friday-morning") {
             messageService.sendFridayMorningReminderAgain()
+            call.respond(HttpStatusCode.OK)
         }
         post("friday-evening") {
             messageService.sendFridayEveningReminderAgain()
+            call.respond(HttpStatusCode.OK)
         }
         post("sunday-morning") {
             messageService.sendSundayMorningReminderAgain()
+            call.respond(HttpStatusCode.OK)
         }
     }
 }
