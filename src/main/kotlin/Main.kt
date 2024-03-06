963fa2977fcd2c37e6413439682bff9574d6ccd4
cotrin1208 <gummy8672@gmail.com>
2024-03-07 08:04:50 +0900

    fix: エントリポイントの修正

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,8 +8,10 @@ import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 
-fun main(args: Array<String>) {
-    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module).start(wait = true)
+fun main() {
+    embeddedServer(CIO, port = 8080, host = "0.0.0.0") {
+        module()
+    }.start(wait = true)
 }
 
 fun Application.module() {
