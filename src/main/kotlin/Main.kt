1fdf4ba484b691d09b34bd0106b66ff478a1a1a5
cotrin1208 <gummy8672@gmail.com>
2024-03-08 13:49:23 +0900

    fix: エントリポイントの修正

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,8 +8,8 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main(args: Array<String>) {
-    embeddedServer(Netty, commandLineEnvironment(args)).start(wait = true)
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") { module() }.start(wait = true)
 }
 
 fun Application.module() {
